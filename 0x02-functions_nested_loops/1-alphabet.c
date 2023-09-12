a99a836590e8c76c5e760cb527b55171049b0367
Yahya <khayoussef.yahya20@gmail.com>
2023-09-12 07:32:25 -0700

    adding mandatory tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - Entry point
+* Description: The function prints alphabet from a to z
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(n);
+		i++;
+	}
+
+	_putchar('\n');
+}
