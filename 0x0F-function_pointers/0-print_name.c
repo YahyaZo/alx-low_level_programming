111564c3baba17a9553cd1d86c8a014a0d66dd06
Yahya <khayoussef.yahya20@gmail.com>
2023-10-11 14:02:03 -0700

    Mandatory tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include<stdlib.h>
+
+
+/**
+ * print_name - A function that prints a name
+ * @name: The name to be printed
+ * @f: A function pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include<stdlib.h>
+
+/**
+ * int_index - gets the index of an array element
+ * @array: array to search through
+ * @size: size of array (num of elements)
+ * @cmp: comparison function to determine if element found
+ *
+ * Return: integer representing index of element found in array
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+
+	}
+	return (-1);
+}
